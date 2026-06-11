f328c3483008b100fff0e3bdb26c0cfd9eaed520
LucasHenrique-Silva <Lucash.s.f74@gmail.com>
2026-06-11 10:31:12 -0300

    finalizando o projeto

--- src/desafio_poo_DIO/Main.java
+++ src/desafio_poo_DIO/Main.java
@@ -24,6 +24,56 @@ public class Main {
 		System.out.println(curso1);
 		System.out.println(curso2);
 		System.out.println(mentoria1);
+		
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp progame");
+		bootcamp.setDescricao("O bootcamp para saber tudo sobre programar");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria1);
+		
+		// ========== CRIAÇÃO E CONFIGURAÇÃO INICIAL ==========
+		DEV dev1 = new DEV();
+		dev1.setNome("Carl");
+
+		DEV dev2 = new DEV();
+		dev2.setNome("Lucas");
+
+		// ========== STATUS INICIAL (antes do bootcamp) ==========
+		System.out.println("=== ANTES DO BOOTCAMP ===");
+		System.out.println("Conteúdos Inscritos " + dev1.getNome() + ": " + dev1.getConteudosIncrito());
+		System.out.println("Conteúdos Inscritos " + dev2.getNome() + ": " + dev2.getConteudosIncrito());
+		System.out.println();
+
+		// ========== INSCRIÇÃO NO BOOTCAMP ==========
+		System.out.println("=== INSCRIÇÕES ===");
+		dev2.inscreverBootcamp(bootcamp);
+		System.out.println(dev2.getNome() + " se inscreveu no bootcamp");
+
+		dev1.inscreverBootcamp(bootcamp);
+		System.out.println(dev1.getNome() + " se inscreveu no bootcamp");
+		System.out.println();
+
+		// ========== STATUS APÓS INSCRIÇÃO ==========
+		System.out.println("=== APÓS INSCRIÇÃO ===");
+		System.out.println("Conteúdos Inscritos " + dev1.getNome() + ": " + dev1.getConteudosIncrito());
+		System.out.println("Conteúdos Inscritos " + dev2.getNome() + ": " + dev2.getConteudosIncrito());
+		System.out.println();
+
+		// ========== AÇÕES DO DEV2 (LUCAS) ==========
+		System.out.println("=== AÇÕES DO " + dev2.getNome().toUpperCase() + " ===");
+		System.out.println("Conteúdos Concluídos: " + dev2.getConteudosConcluidos());
+		System.out.println("Total XP antes de progredir: " + dev2.calcularTotalXp());
+
+		dev2.progredir();
+		System.out.println("\nApós progredir:");
+		System.out.println("Conteúdos Inscritos: " + dev2.getConteudosIncrito());
+		System.out.println("Conteúdos Concluídos: " + dev2.getConteudosConcluidos());
+		System.out.println("Total XP: " + dev2.calcularTotalXp());
+
+
+		
 	}
 
 }
